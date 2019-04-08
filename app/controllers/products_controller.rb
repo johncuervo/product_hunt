abf762f05b4a4427b6e8af81cce5e4575bd083e3
John <johnbaironlr@gmail.com>
2019-04-08 15:00:50 -0500

    Clone Product Hunt

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,26 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path, notice: "El producto ha sido modificado con éxito"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+
+    redirect_to products_path, notice: "El producto fue eliminado con éxito"
+  end
+  
 private
   def product_params
     params.require(:product).permit(:name, :url, :description)
